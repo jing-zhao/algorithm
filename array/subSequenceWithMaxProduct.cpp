81b00d35dd236418f2b83398ddc299054537c1e3
jing-zhao <tomplusplus@gmail.com>
2014-09-12 15:56:37 -0700

    Update subSequenceWithMaxProduct.cpp

--- array/subSequenceWithMaxProduct.cpp
+++ array/subSequenceWithMaxProduct.cpp
@@ -1,29 +1,34 @@
 int maxProduct(int A[], int n) {
-    int maxProd = INT_MIN, cur = 1, negIndex = -1;
+    int maxProd = INT_MIN, cur = 1, firstNegIndex = -1;
     if (n <= 0) return maxProd;
     for (int i = 0; i < n; i++) {
         cur *= A[i];
         maxProd = max(maxProd, cur);
-        if (cur < 0) {
-            if (A[i] < 0) negIndex = i;
-        }
-        else if (cur == 0) {
-            if (negIndex != -1 && negIndex < cur-1) {
-                int prod = 1, index = negIndex+1;
-                while (index < cur) prod *= A[index++];
+        if (A[i] < 0 && firstNegIndex == -1) firstNegIndex = i;
+        if (cur == 0) {
+            if (firstNegIndex != -1 && firstNegIndex < i-1) {
+                int prod = 1, index = firstNegIndex +1;
+                while (index < i) prod *= A[index++];
                 maxProd = max(maxProd, prod);
             }
-            negIndex = -1;
             cur = 1;
-        }
-        else {
-            negIndex = -1;
+            firstNegIndex = -1;
         }
     }
-    if (negIndex != -1 && negIndex < n-1) {
-        int index = negIndex+1, prod = 1;
+    if (firstNegIndex != -1 && firstNegIndex < n-1) {
+        int prod = 1, index = firstNegIndex +1;
         while (index < n) prod *= A[index++];
         maxProd = max(maxProd, prod);
     }
     return maxProd;
+}
+
+int main(int argc, _TCHAR* argv[])
+{
+	int A[] = {4, 7, 2, -3, 1, 0, 7 , -5, 6, -1, 2};
+	// int A[] = {-3, -1, -4, -2, -10};
+	// int A[] = {4, 7, 2, 3, 1};
+	// int A[] = {-1};
+	int ret = maxProduct(A, sizeof(A)/sizeof(int));
+	return 0;
 }
