82151eb126cde43a963169a5e9c05ce7ebf2bfa7
jing-zhao <tomplusplus@gmail.com>
2014-09-12 13:35:28 -0700

    Update lowestCommonAncestor.cpp

--- tree/lowestCommonAncestor.cpp
+++ tree/lowestCommonAncestor.cpp
@@ -1,4 +1,40 @@
 ```c++
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int value) : val(value), left(NULL), right(NULL) {}
+};
+
+TreeNode* deSerializePreOrder(vector<string>& values, int& start) {
+  if (start >= values.size()) return NULL;
+  string val = values[start++];
+  if (val == "#") return NULL;
+  TreeNode* root = new TreeNode(atoi(val.c_str()));
+  root->left = deSerializePreOrder(values, start);
+  root->right = deSerializePreOrder(values, start);
+  return root;
+}
+
+TreeNode* deSerialize(istream& ins) {
+  string word, line;
+  vector<string> words;
+  getline(ins, line);
+  istringstream iss(line);
+  while (!iss.eof()) {
+	iss >> word;
+    words.push_back(word);
+  }
+  int start = 0, last = (int)words.size() - 1;
+  return deSerializePreOrder(words, start);
+  //return deSerializePostOrder(words, last);
+}
+
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* n1, TreeNode* n2, int& covered) {
   covered = 0;
   if (!root) return NULL;
@@ -8,9 +44,30 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* n1, TreeNode* n2, int& 
   if (lCommon) return lCommon;
   TreeNode* rCommon = lowestCommonAncestor(root->right, n1, n2, rCovered);
   if (rCommon) return rCommon;
-  covered += lCommon + rCommon;
+  covered += lCovered + rCovered;
   if (covered == 2) return root;
   else return NULL;
+}
+
+TreeNode* getNode(TreeNode* root, int val) {
+	if (!root) return NULL;
+	if (root->val == val) return root;
+	TreeNode* ret = NULL;
+	ret = getNode(root->left, val);
+	if (ret) return ret;
+	ret = getNode(root->right, val);
+	if (ret) return ret;
+	return NULL;
+}
+
+int main(int argc, _TCHAR* argv[])
+{
+	TreeNode* root = deSerialize(cin);
+	int a, b, covered;
+	cin >> a;
+	cin >> b;
+	TreeNode* ret = lowestCommonAncestor(root, getNode(root, a), getNode(root, b), covered);
+	return 0;
 }
 ```
 
