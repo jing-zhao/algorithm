2589278b7838bc59f5d1a14bd619556fdf22c275
jing-zhao <tomplusplus@gmail.com>
2014-09-22 16:01:00 -0700

    Update flipTreeWithLeafRightNodes.cpp

--- tree/flipTreeWithLeafRightNodes.cpp
+++ tree/flipTreeWithLeafRightNodes.cpp
@@ -15,3 +15,28 @@ TreeNode* flip(TreeNode* root, TreeNode*& rightMost) {
 	  root->left = root->right = 0;
     return leftRoot;
 }
+
+TreeNode* flipTree(TreeNode* root) {
+    if (!root) return root;
+    stack<TreeNode*> st;
+    while (root) {
+        st.push(root);
+        root = root->left;
+    }
+    TreeNode* prev = 0;
+	root = 0;
+    while (!st.empty()) {
+        TreeNode* n = st.top();
+        st.pop();
+        if (!prev) {
+            root = n;
+        }
+        else {
+            prev->left = n->right;
+            prev->right = n;
+        }
+        prev = n;
+    }
+    prev->left = prev->right = 0;
+    return root;
+}
